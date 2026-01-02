6957bb1acf4f1334da233ad72eba9df0462f98fe
Otniel Marques <otnielmarques3@gmail.com>
2026-01-02 01:07:21 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,5 +40,12 @@ public class Program {
                 department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+
+        System.out.println("\n=== TEST 5: seller update===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Waine");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
